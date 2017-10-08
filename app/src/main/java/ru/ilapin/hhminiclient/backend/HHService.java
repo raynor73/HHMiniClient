9d5c46a1a4312831656d7e47a3843d71e9b6b896
Igor Lapin <raynor73@gmail.com>
2017-10-08 23:03:32 +0300

    Add Vacancy Detail request to Backend

--- app/src/main/java/ru/ilapin/hhminiclient/backend/HHService.java
+++ app/src/main/java/ru/ilapin/hhminiclient/backend/HHService.java
@@ -4,6 +4,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 interface HHService {
@@ -11,4 +12,8 @@ interface HHService {
 	@Headers("User-Agent: HHMiniClient/1.0 (igor.lapin73@gmail.com)")
 	@GET("vacancies")
 	Call<ResponseBody> vacancies(@Query("text") String text);
+
+	@Headers("User-Agent: HHMiniClient/1.0 (igor.lapin73@gmail.com)")
+	@GET("GET /vacancies/{vacancyId}")
+	Call<ResponseBody> vacancy(@Path("vacancyId") String vacancyId);
 }
